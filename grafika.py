29e64970216e613ab8edcb186821ae89564d1968
nereagurru <78503555+nereagurru@users.noreply.github.com>
2021-04-29 15:58:34 +0200

    Update grafika.py

--- grafika.py
+++ grafika.py
@@ -7,7 +7,6 @@ Created on Wed Mar 17 18:03:57 2021
 
 import matplotlib.pyplot as plt
 import numpy as np
-from skyfield.api import load
 
 
 #Gasaren dentsitatea
@@ -44,9 +43,11 @@ hautsa_plan = dent_hauts(dist_plan)
 fig, (ax_den, ax_ten) = plt.subplots(nrows=2, ncols=1, sharex=True)
 
 
+# Grafikan textua jarri
 ax_den.set_title('Gainazal dentsitatea')
 ax_den.set_ylabel(r'$\Sigma \; (g\,cm^{-2})$')
 ax_den.set_yscale('log')
+ax_den.legend()
 
 ax_ten.set_title('Tenperatura')
 ax_ten.set_xlabel('r (AU)')
@@ -54,21 +55,23 @@ ax_ten.set_ylabel('T (K)')
 ax_ten.set_xscale('log')
 ax_ten.set_yscale('log')
 
-
+# Grafikatu
 ax_den.plot(r, gas_list, label='Gasa')
 ax_den.plot(r, hauts_list, label='Hautsa')
 ax_den.scatter(dist_plan, hautsa_plan)
 
+# Planetetan izenak jarri
 for i, letra in enumerate(letra):
     ax_den.annotate(letra, (dist_plan[i], hautsa_plan[i]), (1.1*dist_plan[i],
                     hautsa_plan[i]), size=7)
 
+# Materia kondentsatuko deneko distantzia adierazteko
 t = np.linspace(np.amin(tenp_list), np.amax(tenp_list), 100)
 sigma = np.linspace(np.amin(hauts_list), np.amax(gas_list), 100)
 ax_ten.plot(np.full(100, r_kon), t, linestyle='--', color='grey')
 ax_ten.scatter(r_kon, tenp(r_kon))
 ax_ten.annotate('170 K', (r_kon, tenp(r_kon)), (1.1*r_kon, tenp(r_kon)))
 ax_den.plot(np.full(100, r_kon), sigma, linestyle='--', color='grey')
-ax_den.legend()
 ax_ten.plot(r, tenp_list)
+
 plt.show()
